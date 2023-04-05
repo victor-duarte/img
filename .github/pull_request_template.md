18d6147820834c75e61cb4d1fc848f0430fde671
Victor Duarte <vicsalf@gmail.com>
2023-04-05 02:05:05 -0500

    Setup Next.js

---
+++ .github/pull_request_template.md
@@ -0,0 +1,9 @@
+# Description
+
+Include a summary of the change and which issue is fixed.
+
+## Changelog
+
+List any dependencies that are required for this change.
+
+## Screenshots
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Welcome IMG</h1>
+      <p>A tool to help international medical graduates in their journey.</p>
+    </main>
+  );
+}
